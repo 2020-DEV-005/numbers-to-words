99b4f5f2b3eccf072b278ef5a095a9d6dbfd19bc
2020-DEV-005 <2020.dev.005@gmail.com>
2020-01-11 12:05:22 +0000

    App renders the Converter component

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import {AppConst} from '../../constants/App.const';
+import Converter from '../Converter/index';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <h1>{AppConst.APP_TITLE}</h1>
+          <h1>{AppConst.APP_TITLE}</h1>
       </header>
+        <Converter />
     </div>
   );
 }
--- src/components/App/index.test.js
+++ src/components/App/index.test.js
@@ -3,11 +3,17 @@ import { render } from '@testing-library/react';
 import {shallow} from 'enzyme'; 
 import App from './index';
 import {AppConst} from '../../constants/App.const';
+import Converter from '../Converter/';
 
 describe(("<App/> component"), () => {
+    
     it("App should have the title", () => {
         let wrapper = shallow(<App/>);
         expect(wrapper.find("header h1").text()).toEqual(AppConst.APP_TITLE);     
     });
 
+    it("App should load Converter component", () => {
+        let wrapper = shallow(<App/>);
+        expect(wrapper.find("Converter")).toBeDefined();
+    });
 });
